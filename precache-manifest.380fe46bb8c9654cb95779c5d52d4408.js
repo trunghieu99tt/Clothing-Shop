998ca1a8933db55a2b226763b043161ba47035d1
Hieu <trunghieu99tt@gmail.com>
2020-02-28 10:32:26 +0700

    Updates

---
+++ precache-manifest.380fe46bb8c9654cb95779c5d52d4408.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d849b5b2265c45e31101abd1909419",
+    "url": "/Clothing-Shop/index.html"
+  },
+  {
+    "revision": "efb908a075071b2381e0",
+    "url": "/Clothing-Shop/static/css/2.e016cb85.chunk.css"
+  },
+  {
+    "revision": "a7f93028ffb8c5953cee",
+    "url": "/Clothing-Shop/static/css/main.f97e6096.chunk.css"
+  },
+  {
+    "revision": "efb908a075071b2381e0",
+    "url": "/Clothing-Shop/static/js/2.e313d1cb.chunk.js"
+  },
+  {
+    "revision": "7f659f12e1ada3b78a08d9c3a87f5e92",
+    "url": "/Clothing-Shop/static/js/2.e313d1cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7f93028ffb8c5953cee",
+    "url": "/Clothing-Shop/static/js/main.cf896f43.chunk.js"
+  },
+  {
+    "revision": "d2efbe5cf55ac2d29f39",
+    "url": "/Clothing-Shop/static/js/runtime-main.5665197c.js"
+  }
+]);
